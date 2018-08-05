4c311ad44d5b2c2eb8390d5e7fb86e329cbceb2c
Administrator <Administrator@1-PC.LCSMK6>
2018-08-05 21:32:20 +0800

    增加获取当前时间方法（yyyy-MM-dd HH:mm:ss格式）

--- src/com/zjx/util/TimeUtil.java
+++ src/com/zjx/util/TimeUtil.java
@@ -1,6 +1,8 @@
 package com.zjx.util;
 
-import org.junit.Test;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 
 public class TimeUtil {
 
@@ -10,6 +12,23 @@ public class TimeUtil {
 	 */
 	public long getCurrentTime() {
 		return System.currentTimeMillis();
+	}
+	
+	/***
+	 * 获取当前时间（yyyy-MM-dd HH:mm:ss格式）
+	 * @return
+	 */
+	public String getFormatTime() {
+		Date date=new Date();
+		String format="yyyy-MM-dd HH:mm:ss";
+		SimpleDateFormat sdf=new SimpleDateFormat(format);
+		return sdf.format(date);
+	}
+	
+	public static void main(String[] args) {
+		TimeUtil util=new TimeUtil();
+		System.out.println(util.getFormatTime());
+		
 	}
 	
 }
