96a3788f112f43083ba83c629995404d891da8aa
Administrator <Administrator@1-PC.LCSMK6>
2018-08-05 21:18:20 +0800

    增加获取时间方法

---
+++ src/com/zjx/util/TimeUtil.java
@@ -0,0 +1,15 @@
+package com.zjx.util;
+
+import org.junit.Test;
+
+public class TimeUtil {
+
+	/***
+	 * 获取当前时间（long格式）
+	 * @return
+	 */
+	public long getCurrentTime() {
+		return System.currentTimeMillis();
+	}
+	
+}
